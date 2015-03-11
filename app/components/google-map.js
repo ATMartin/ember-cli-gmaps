7c75ca6aa064980010039ef9882f5176c8101d9b
Alex Martin <martin.alex.t+github@gmail.com>
2015-03-10 23:40:15 -0400

    Add component file

--- app/components/google-map.js
+++ app/components/google-map.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  insertMap: function() {
+    var container = this.$('.map-canvas')[0];
+    var options = {
+      center: new window.google.maps.LatLng(
+        this.get('latitude'), 
+        this.get('longitude')
+      ),
+      zoom: 15
+    };
+    new window.google.maps.Map(container, options);
+  }.on('didInsertElement')
 });
